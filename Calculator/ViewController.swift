a93aec1916a99c1a3aa176a8dfb331ed4d40f10a
Sasa Gvozdenovic <sasagvozdenovic@hotmail.com>
2018-02-05 01:03:01 +0100

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Sasa Gvozdenovic on 05/02/2018.
+//  Copyright © 2018 Sasa Gvozdenovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
